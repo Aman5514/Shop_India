059a1fc40472ae99f4479584a2e0e33602bff8dc
Aman Gupta <aman930530@gmail.com>
2021-07-28 00:56:11 +0530

    fixing bugs

--- middleware/protected.js
+++ middleware/protected.js
@@ -4,8 +4,9 @@ const {Registration} = require("../database/model");
 const protected = async (req, res, next) => {
   try {
     const token = req.cookies.ShopIndiaCookie;
-    const verify = jwt.verify(token, process.env.SECRET_KEY);
+    const verify = jwt.verify(token,"mynameisamangutaandiwanttobecomesuccessfulldeveloper");
     const user = await Registration.findOne({_id:verify._id});
+
     if(verify && user.email === "amang5514@gmail.com")
     {
         next();
